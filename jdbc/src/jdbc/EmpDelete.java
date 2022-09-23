110fcc9764b8352b549ed02a87b899c5f7c28483
Eshita2403 <eeshitamahant@gmail.com>
2022-09-23 21:41:53 +0530

    comment added commit

--- jdbc/src/jdbc/EmpDelete.java
+++ jdbc/src/jdbc/EmpDelete.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+// this is my comment
 public class EmpDelete {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
